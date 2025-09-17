2bb7a0920365c1830b50b28c22f0de6be7a24666
Ishara Sahan Kaushalya <sahankaushalya.info@gmail.com>
2025-09-17 19:09:17 +0530

    Updating CircleShapes

--- src/components/CircleShape.tsx
+++ src/components/CircleShape.tsx
@@ -1,35 +1,36 @@
 import { View } from "react-native";
 
 interface Circle{
-width:number,
-height:number,
-fillColor:string,
-borderRadius:number
-topValue?:number,
-leftValue?:number,
-rightValue?:number,
-bottomValue?:number,
-borderColor?:string,
-borderWidth?:number,
-opacity?:number
+width:number;
+height:number;
+borderRadius:number;
+fillColor?:string;
+className?:string;
+topValue?:number;
+leftValue?:number;
+rightValue?:number;
+bottomValue?:number;
+borderColor?:string;
+borderWidth?:number;
+opacity?:number;
 }
 
-export default function CircleShape({width,height,fillColor,borderRadius,topValue,leftValue,rightValue,bottomValue,
-    borderColor,borderWidth,opacity}:Circle) {
+export default function CircleShape(c:Circle) {
     return (
-        <View style={{
-            width:width,
-            height:height,
-            backgroundColor:fillColor,
-            borderRadius:borderRadius,
+        <View className={`${c.className ?? ""}`} style={{
+            width:c.width,
+            height:c.height,
+            borderRadius:c.borderRadius,
             position:'absolute',
-            ...(topValue !== undefined && {top:topValue}),
-            ...(leftValue !== undefined && {left:leftValue} ),
-            ...(rightValue !== undefined && {right:rightValue} ),
-            ...(bottomValue !== undefined && {bottom:bottomValue}),
-            ...(borderColor !== undefined && {borderColor:borderColor} ),
-            ...(borderWidth !== undefined && {borderWidth:borderWidth}),
-            ...(opacity !== undefined && {opacity:opacity}),
+            ...(c.fillColor !== undefined && {backgroundColor:c.fillColor}),
+            ...(c.topValue !== undefined && {top:c.topValue}),
+            ...(c.leftValue !== undefined && {left:c.leftValue} ),
+            ...(c.rightValue !== undefined && {right:c.rightValue} ),
+            ...(c.bottomValue !== undefined && {bottom:c.bottomValue}),
+            ...(c.borderColor !== undefined && {borderColor:c.borderColor} ),
+            ...(c.borderWidth !== undefined && {borderWidth:c.borderWidth}),
+            ...(c.opacity !== undefined && {opacity:c.opacity}),
+            zIndex:0,
         }}>
 
         </View>
